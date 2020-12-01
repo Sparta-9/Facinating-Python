aa7158e0ac217313c0f8a7e8ea5bdf3876164dac
vikram singh <58397405+Sparta-9@users.noreply.github.com>
2020-12-01 22:56:15 +0530

    Update 102. Virtual.py

--- 102. Virtual.py
+++ 102. Virtual.py
@@ -5,3 +5,10 @@ assistant.runAndWait()
 
 
 
+#If you wants to take user input ther code be like
+
+import pyttsx3
+assistant = pyttsx3.init()
+user = input(" Say something : ")
+assistant.say(user)
+assistant.runAndWait()
